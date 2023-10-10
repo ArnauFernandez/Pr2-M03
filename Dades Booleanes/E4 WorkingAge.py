6b49cde3e2ff8e1ea9c9f1fd216191f074d18543
ArnauFernandez <arnau.fernandez.7e4@itb.cat>
2023-10-10 11:28:23 +0200

    e1 i e4 hecho

---
+++ Dades Booleanes/E4 WorkingAge.py
@@ -0,0 +1,13 @@
+"""
+Arnau Fernandez, Adrian Zamora i Matteo Vilchez
+10/10/2023
+ASIXc1C M03 UF1
+Pr2_TipusDades
+Exercici WorkingAge
+"""
+askAge=int(input("Dime tu edad: "))
+
+if askAge >=16:
+    print("tienes edad legal para trabajar")
+elif askAge>=65:
+    print("no tienes edad para trabajar es hora de jubilarse")
---
+++ Dades Numèriques/e1.HowBigIsMyPizza.py
@@ -0,0 +1,13 @@
+"""
+Matteo vilchez
+Adrian Zamora
+Arnau Fernandez
+10/10/2023
+Pr2 M03 UF1
+e1.HowBigIsMyPizza
+"""
+import math
+radi=float(input(""))
+diametre=radi**2
+areaCercle=diametre*math.pi
+print("La superfície de la pizza es ",areaCercle)
