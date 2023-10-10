5ec27375ff93978850202ba33c8564f2bc6255f5
ArnauFernandez <arnau.fernandez.7e4@itb.cat>
2023-10-10 11:22:27 +0200

    e1 hecho

--- Dades Caràcter/e5 Encript12345.py
+++ Dades Caràcter/e5 Encript12345.py
@@ -2,12 +2,12 @@
 Matteo vilchez
 Adrian Zamora
 Arnau Fernandez
-11/10/2023
+10/10/2023
 Pr2 M03 UF1
 e1.HowBigIsMyPizza
 """
 import math
-radi=int(input(""))
+radi=float(input(""))
 diametre=radi**2
 areaCercle=diametre*math.pi
 print("La superfície de la pizza es ",areaCercle)
