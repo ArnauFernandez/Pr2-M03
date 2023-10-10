560518b8de2ee3d156f2def1c2ed716ac745b455
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2023-10-10 11:28:57 +0200

    Update e5 Encript12345.py

---
+++ Dades Caràcter/e5 Encript12345.py
@@ -0,0 +1,19 @@
+"""
+Arnau Fernandez, Adrian Zamora i Matteo Vilchez
+10/10/2023
+ASIXc1C M03 UF1
+Pr2_TipusDades
+Exercici Encript12345
+"""
+encriptar=str(input("Escribe una palabra:"))
+encriptar=encriptar.replace("a","1")
+encriptar=encriptar.replace("A","1")
+encriptar=encriptar.replace("e","2")
+encriptar=encriptar.replace("E","2")
+encriptar=encriptar.replace("i","3")
+encriptar=encriptar.replace("I","3")
+encriptar=encriptar.replace("o","4")
+encriptar=encriptar.replace("O","4")
+encriptar=encriptar.replace("u","5")
+encriptar=encriptar.replace("U","5")
+print(encriptar)
