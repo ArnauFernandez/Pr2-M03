e48092e772ce62f1aa27c931f8e7c8f235ff0685
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2023-10-10 11:18:52 +0200

    Update E3 InRange.py

---
+++ Dades Booleanes/E3 InRange.py
@@ -0,0 +1,14 @@
+"""
+Arnau Fernandez, Adrian Zamora i Matteo Vilchez
+10/10/2023
+ASIXc1C M03 UF1
+Pr2_TipusDades
+Exercici InRange
+"""
+numero1=int(input("Pídeme el primer numero: "))
+numero2=int(input("Pídeme el segundo numero: "))
+numero3=int(input("Pídeme el tercer numero: "))
+numero4=int(input("Pídeme el cuarto numero: "))
+numero5=int(input("Pídeme el ultimo numero: "))
+print(numero5 >=numero1 and numero5 <=numero2 and numero5 >=numero3 and numero5 <=numero4)
+
