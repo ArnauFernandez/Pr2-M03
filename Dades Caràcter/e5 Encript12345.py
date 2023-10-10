c20cabf90654f518f6ff12bc5a40e671eccce142
ArnauFernandez <arnau.fernandez.7e4@itb.cat>
2023-10-10 10:40:22 +0200

    e1 hecho

---
+++ Dades Caràcter/e5 Encript12345.py
@@ -0,0 +1,13 @@
+"""
+Matteo vilchez
+Adrian Zamora
+Arnau Fernandez
+11/10/2023
+Pr2 M03 UF1
+e1.HowBigIsMyPizza
+"""
+import math
+radi=int(input(""))
+diametre=radi**2
+areaCercle=diametre*math.pi
+print("La superfície de la pizza es ",areaCercle)
